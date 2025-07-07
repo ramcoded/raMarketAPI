14cc452a6a55b191daa004454b861bef0000fe64
Ram <roymarven9@gmail.com>
2025-07-07 10:03:23 +0800

    initial upload

---
+++ src/main/java/com/raMarket/raMarketAPI/RaMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.raMarket.raMarketAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaMarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raMarket/raMarketAPI/RaMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raMarket.raMarketAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
